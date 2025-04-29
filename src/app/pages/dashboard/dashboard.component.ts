29e86746773a6bcba13312301f7689d41624d9a0
RB-Fashion <rbfunit1@gmail.com>
2025-04-29 11:25:38 +0530

    env locally working

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  apiUrl = 'http://localhost:5000';  // Add the apiUrl property
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsersList();
+  }
+
+  getUsersList() {
+    this.http.get(`${this.apiUrl}/users/list`)
+      .subscribe(
+        response => {
+          console.log('Users list:', response);
+        },
+        error => {
+          console.error('Error:', error);
+        }
+      );
+  }
 }
